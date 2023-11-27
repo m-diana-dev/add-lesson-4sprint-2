aade6912e459a6ee4f6486a6cdba324e5420232e
m-diana-dev <m.diana.dev@gmail.com>
2023-11-27 16:54:35 +0300

    task 3 is done

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -2,14 +2,15 @@ import { Dispatch } from 'redux'
 import { decksAPI, UpdateDeckParams } from './decks-api.ts'
 import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
 import { SetAppStatusAC } from '../../app/app-reducer.ts'
+import { isAxiosError } from 'axios'
 
 export const fetchDecksTC = () => async (dispatch: Dispatch) => {
   dispatch(SetAppStatusAC('loading'))
-  try{
+  try {
     const res = await decksAPI.fetchDecks()
     dispatch(setDecksAC(res.data.items))
     dispatch(SetAppStatusAC('succeeded'))
-  } catch(e) {
+  } catch (e) {
     dispatch(SetAppStatusAC('failed'))
   }
 }
@@ -26,8 +27,26 @@ export const deleteDeckTC = (id: string) => (dispatch: Dispatch) => {
   })
 }
 
-export const updateDeckTC = (params: UpdateDeckParams) => (dispatch: Dispatch) => {
-  return decksAPI.updateDeck(params).then((res) => {
-    dispatch(updateDeckAC(res.data))
-  })
+export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
+  try {
+    // throw new Error('error!!!')
+    const res = await decksAPI.updateDeck(params)
+      dispatch(updateDeckAC(res.data))
+  } catch (e) {
+    let errorMessage: string;
+
+    if(isAxiosError<ServerError>(e)){
+       errorMessage = e.response ? e.response.data.errorMessages[0].message : e.message
+    } else {
+      errorMessage = (e as Error).message
+    }
+    console.log(errorMessage)
+  }
+}
+
+type ServerError = {
+  errorMessages: {
+    field: string
+    message: string
+  }[]
 }
